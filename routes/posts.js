a0e1e67a7fa83c33e3dd71def05cbce8ac898759
brianschnee <brian.a.schnee@gmail.com>
2022-09-11 19:38:59 -0400

    Feat: adding createPost and deletePost routes

--- routes/posts.js
+++ routes/posts.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const router = express.Router()
+const upload = require("../middleware/multer");
 const { ensureAuth } = require('../middleware/auth')
 
-
+router.post("/createPost", upload.single("file"), postsController.createPost);
+router.delete("/deletePost/:id", postsController.deletePost);
 
 //export
 module.exports = router
