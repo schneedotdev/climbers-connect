746e1c03a151ebbe6cceacc2cd01a66334308a0d
brianschnee <brian.a.schnee@gmail.com>
2022-09-11 19:31:22 -0400

    Feat: adding createPost controller

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,8 +1,29 @@
 const User = require('../models/User')
+const Post = require('../models/Post')
 const cloudinary = require("../middleware/cloudinary");
 
 module.exports = {
   getProfile: async (req, res) => {
     console.log('getProfile')
+  },
+  createPost: async (req, res) => {
+    try {
+      // Upload image to cloudinary
+      const result = await cloudinary.uploader.upload(req.file.path);
+
+      await Post.create({
+        title: req.body.title,
+        image: result.secure_url,
+        cloudinaryId: result.public_id,
+        caption: req.body.caption,
+        likes: 0,
+        user: req.user.id,
+      });
+
+      console.log("Post has been added!");
+      res.redirect("/profile");
+    } catch (err) {
+      console.log(err);
+    }
   },
 }
