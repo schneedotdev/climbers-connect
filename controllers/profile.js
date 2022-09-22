0949060818c8d6301b8c99177c4b76c3c2c8f069
brianschnee <brian.a.schnee@gmail.com>
2022-09-21 23:02:16 -0400

    updating conditional for when errors are thrown, fixed cloudinary destroy bug

--- controllers/profile.js
+++ controllers/profile.js
@@ -11,28 +11,26 @@ module.exports = {
                 const user = await User.findOne({ username: req.params.username }).lean()
 
                 // DISPLAY ERROR IF THE USER INPUTS A URL THATS NOT AN ACTUAL USER
-                if (user) {
-                    const climbs = await Climb.find({ user: req.user.id })
-                    const partnerSearches = await PartnerSearch.find({ user: req.user.id })
+                if (!user) throw 'User does not exist'
 
-                    // check to see if the current user is requesting their own profile
-                    const isCurrentUser = req.user.username === req.params.username
+                const climbs = await Climb.find({ user: req.user.id })
+                const partnerSearches = await PartnerSearch.find({ user: req.user.id })
 
-                    let following = false;
-                    if (!isCurrentUser) {
-                        // check to see if the user is following the account they are requesting
-                        following = req.user.profile.following.some(userId => userId.toString() === user._id.toString())
-                    }
+                // check to see if the current user is requesting their own profile
+                const isCurrentUser = req.user.username === req.params.username
 
-                    const { twitter, avatar: { url } } = user.profile
-
-                    res.render('profile', { user, climbs, partnerSearches, isCurrentUser, following, twitter, url })
-                } else {
-                    throw 'User does not exist'
+                let following = false;
+                if (!isCurrentUser) {
+                    // check to see if the user is following the account they are requesting
+                    following = req.user.profile.following.some(userId => userId.toString() === user._id.toString())
                 }
+
+                const { twitter, avatar: { url } } = user.profile
+
+                res.render('profile', { user, climbs, partnerSearches, isCurrentUser, following, twitter, url })
             }
         } catch (err) {
-            console.err(error)
+            console.error(err)
             res.sendStatus(404)
         }
     },
@@ -40,11 +38,9 @@ module.exports = {
         try {
             const isCurrentUser = req.user.username === req.params.username
 
-            if (isCurrentUser) {
-                res.render('edit-profile', { user: req.user })
-            } else {
-                throw 'Attempting to retreieve edit profile page of an account that doesnt belong to user'
-            }
+            if (!isCurrentUser) throw 'User does not have permissions to access this page'
+
+            res.render('edit-profile', { user: req.user })
         } catch (err) {
             console.error(err)
             res.redirect(`/user/${req.user.username}`)
@@ -52,20 +48,18 @@ module.exports = {
     },
     updateProfile: async (req, res) => {
         try {
-            if (req.params.username === req.user.username) {
-                const user = await User.findOne({ _id: req.user._id })
-                const { name, location, about, twitter } = req.body;
+            if (req.params.username !== req.user.username) throw 'User does not have edit permissions for this profile'
 
-                if (name) user.profile.name = name.trim()
-                if (location) user.profile.location = location.trim()
-                if (about) user.profile.about = about.trim()
-                if (twitter) user.profile.twitter = twitter.trim().replaceAll(' ', '')
+            const user = await User.findOne({ _id: req.user._id })
+            const { name, location, about, twitter } = req.body;
 
-                await user.save()
-                res.redirect(`/user/${req.user.username}`)
-            } else {
-                throw 'Attempting to update profile of an account that doesnt belong to user'
-            }
+            if (name) user.profile.name = name.trim()
+            if (location) user.profile.location = location.trim()
+            if (about) user.profile.about = about.trim()
+            if (twitter) user.profile.twitter = twitter.trim().replaceAll(' ', '')
+
+            await user.save()
+            res.redirect(`/user/${req.user.username}`)
         } catch (err) {
             console.error(err)
             res.redirect(`/user/edit/${req.user.username}`)
@@ -73,30 +67,30 @@ module.exports = {
     },
     updateAvatar: async (req, res) => {
         try {
-            if (req.params.username === req.user.username) {
-                // Upload image to cloudinary
-                const { secure_url, public_id } = await cloudinary.uploader.upload(req.file.path)
+            if (req.params.username !== req.user.username) throw 'User does not have edit permissions for this profile'
 
-                // check to see if a new url and id were provided
-                if (secure_url && public_id) {
-                    const user = await User.findOne({ _id: req.user._id })
+            // Upload image to cloudinary
+            const { secure_url, public_id } = await cloudinary.uploader.upload(req.file.path)
 
-                    // Delete previous avatar from cloudinary
-                    await cloudinary.uploader.destroy(user.profile.avatar.id)
+            // check to see if a new url and id were provided
+            if (secure_url && public_id) {
+                const user = await User.findOne({ _id: req.user._id })
+                console.log('avatar:', user.profile.avatar)
 
-                    // assign new url and id
-                    user.profile.avatar.url = secure_url
-                    user.profile.avatar.id = public_id
+                if (user.profile.avatar.id) await cloudinary.uploader.destroy(user.profile.avatar.id)
 
-                    await user.save()
-                }
-                res.redirect(`/user/edit/${req.user.username}`)
-            } else {
-                throw 'Attempting to update avatar of an account that doesnt belong to user'
+                // assign new url and id
+                user.profile.avatar.url = secure_url
+                user.profile.avatar.id = public_id
+
+                // Delete previous avatar from cloudinary
+                await user.save()
             }
+
+            res.redirect(`/user/edit/${req.user.username}`)
         } catch (err) {
             console.error(err)
-            res.redirect(`/user/edit/${req.user.username}`)
+            res.redirect(`/user/${req.user.username}`)
         }
     },
     follow: async (req, res) => {
