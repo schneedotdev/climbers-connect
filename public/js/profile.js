818630836764394f93f9a53ba88ba29dedf76e95
brianschnee <brian.a.schnee@gmail.com>
2022-09-22 00:33:09 -0400

    Create post overlay pops up on btn click

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,9 +1,23 @@
 const followingBtn = document.querySelector('.following--profile-button')
+const createPost = document.querySelector('.create--post')
+const createPostWrapper = document.querySelector('.create--post-wrapper')
 
-followingBtn.addEventListener('mouseover', () => {
-    followingBtn.innerText = 'Unfollow'
+if (followingBtn) {
+    followingBtn.addEventListener('mouseover', () => {
+        followingBtn.innerText = 'Unfollow'
+    })
+
+    followingBtn.addEventListener('mouseout', () => {
+        followingBtn.innerText = 'Following'
+    })
+}
+
+createPost.addEventListener('click', () => {
+    createPostWrapper.style.display = 'block'
 })
 
-followingBtn.addEventListener('mouseout', () => {
-    followingBtn.innerText = 'Following'
+createPostWrapper.addEventListener('click', (e) => {
+    if (e.target.className === 'create--post-wrapper') {
+        createPostWrapper.style.display = 'none'
+    }
 })
