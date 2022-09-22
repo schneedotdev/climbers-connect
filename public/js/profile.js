c29ca81500015a2964d227526c217df1da6ee991
brianschnee <brian.a.schnee@gmail.com>
2022-09-22 18:55:57 -0400

    Fix: adding changes to create post overlay

--- public/js/profile.js
+++ public/js/profile.js
@@ -3,6 +3,8 @@ const createPost = document.querySelector('.create--post')
 const createPostWrapper = document.querySelector('.create--post-wrapper')
 const climbPost = document.querySelector('.climb-post')
 const partnerPost = document.querySelector('.partner-post')
+const climbForm = document.querySelector('.climb--form')
+const connectForm = document.querySelector('.connect--form')
 
 if (followingBtn) {
     followingBtn.addEventListener('mouseover', () => {
@@ -24,64 +26,20 @@ createPostWrapper.addEventListener('click', (e) => {
     }
 })
 
-const climbTitle = document.querySelector('.post--climb-title')
-const climbHR = document.querySelector('.post--climb-hr')
-const partnerTitle = document.querySelector('.post--partner-title')
-const partnerHR = document.querySelector('.post--partner-hr')
 let lastClicked = 'climb'
 
 climbPost.addEventListener('click', () => {
     lastClicked = 'climb'
-    climbTitle.style.color = '#A0A1F5'
-    climbHR.style.borderColor = '#A0A1F5'
-    partnerTitle.style.color = '#fff'
-    partnerHR.style.borderColor = '#fff'
-})
-
-climbPost.addEventListener('mouseover', () => {
-    if (lastClicked === 'climb') {
-        climbTitle.style.color = '#383970'
-        climbHR.style.borderColor = '#383970'
-    } else {
-        climbTitle.style.color = '#babebf'
-        climbHR.style.borderColor = '#babebf'
-    }
-})
-
-climbPost.addEventListener('mouseout', () => {
-    if (lastClicked === 'climb') {
-        climbTitle.style.color = '#A0A1F5'
-        climbHR.style.borderColor = '#A0A1F5'
-    } else {
-        climbTitle.style.color = '#fff'
-        climbHR.style.borderColor = '#fff'
-    }
+    climbPost.style.background = '#A0A1F5'
+    partnerPost.style.background = 'none'
+    climbForm.style.display = "flex"
+    connectForm.style.display = "none"
 })
 
 partnerPost.addEventListener('click', () => {
     lastClicked = 'partner'
-    climbTitle.style.color = '#fff'
-    climbHR.style.borderColor = '#fff'
-    partnerTitle.style.color = '#81AEE5'
-    partnerHR.style.borderColor = '#81AEE5'
-})
-
-partnerPost.addEventListener('mouseover', () => {
-    if (lastClicked === 'partner') {
-        partnerTitle.style.color = '#274469'
-        partnerHR.style.borderColor = '#274469'
-    } else {
-        partnerTitle.style.color = '#babebf'
-        partnerHR.style.borderColor = '#babebf'
-    }
-})
-
-partnerPost.addEventListener('mouseout', () => {
-    if (lastClicked === 'partner') {
-        partnerTitle.style.color = '#81AEE5'
-        partnerHR.style.borderColor = '#81AEE5'
-    } else {
-        partnerTitle.style.color = '#fff'
-        partnerHR.style.borderColor = '#fff'
-    }
+    climbPost.style.background = 'none'
+    partnerPost.style.background = '#A0A1F5'
+    connectForm.style.display = "flex"
+    climbForm.style.display = "none"
 })
