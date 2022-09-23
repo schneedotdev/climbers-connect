2d975ae57245d1bd35fc761d55a0851958d27197
brianschnee <brian.a.schnee@gmail.com>
2022-09-23 00:11:21 -0400

    Feat: adding star rating system to form, styled hover and click events

--- public/js/profile.js
+++ public/js/profile.js
@@ -27,19 +27,69 @@ createPostWrapper.addEventListener('click', (e) => {
 })
 
 let lastClicked = 'climb'
+const climbTitle = document.querySelector('.post--climb-title')
+const partnerTitle = document.querySelector('.post--partner-title')
+const [purple, darkPurple, white, lightGray] = ['#A0A1F5', '#383970', '#fff', '#babebf']
 
 climbPost.addEventListener('click', () => {
     lastClicked = 'climb'
-    climbPost.style.background = '#A0A1F5'
+    climbPost.style.background = purple
     partnerPost.style.background = 'none'
     climbForm.style.display = "flex"
     connectForm.style.display = "none"
+    partnerTitle.style.color = lightGray
+})
+
+climbPost.addEventListener('mouseover', () => {
+    if (lastClicked === 'climb') {
+        climbTitle.style.color = darkPurple
+    } else {
+        climbTitle.style.color = white
+    }
+})
+
+climbPost.addEventListener('mouseout', () => {
+    if (lastClicked === 'climb') {
+        climbTitle.style.color = white
+    } else {
+        climbTitle.style.color = lightGray
+    }
 })
 
 partnerPost.addEventListener('click', () => {
     lastClicked = 'partner'
     climbPost.style.background = 'none'
-    partnerPost.style.background = '#A0A1F5'
+    partnerPost.style.background = purple
     connectForm.style.display = "flex"
     climbForm.style.display = "none"
+    climbTitle.style.color = lightGray
+})
+
+partnerPost.addEventListener('mouseover', () => {
+    if (lastClicked === 'partner') {
+        partnerTitle.style.color = darkPurple
+    } else {
+        partnerTitle.style.color = white
+    }
+})
+
+partnerPost.addEventListener('mouseout', () => {
+    if (lastClicked === 'partner') {
+        partnerTitle.style.color = white
+    } else {
+        partnerTitle.style.color = lightGray
+    }
+})
+
+const stars = Array.from(document.querySelectorAll('.star')).reverse()
+
+stars.forEach((star, i) => {
+    star.addEventListener('click', () => {
+        stars.forEach(star => star.style.color = white)
+
+        // color stars before
+        for (let j = 0; j <= i; j++) {
+            stars[j].style.color = purple
+        }
+    })
 })
