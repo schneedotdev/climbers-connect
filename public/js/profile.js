7df0535f94ae93ba3d50f96a384291f299ee8f8f
brianschnee <brian.a.schnee@gmail.com>
2022-10-08 18:03:12 -0400

    Fix: updating client-side js to reflect recent changes

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,16 +1,9 @@
 const followingBtn = document.querySelector('.following--profile-button')
 const createPost = document.querySelector('.create--post')
 const createPostWrapper = document.querySelector('.create--post-wrapper')
-const climbPost = document.querySelector('.climb-post')
-const partnerPost = document.querySelector('.partner-post')
-const climbForm = document.querySelector('.climb--form')
-const connectForm = document.querySelector('.connect--form')
 const stars = Array.from(document.querySelectorAll('.star')).reverse()
 const starInputs = Array.from(document.querySelectorAll('.star--input')).reverse()
-const climbTitle = document.querySelector('.post--climb-title')
-const partnerTitle = document.querySelector('.post--partner-title')
-const [purple, darkPurple, white, lightGray, gold] = ['#A0A1F5', '#383970', '#fff', '#babebf', '#FFE55C']
-
+const [white, gold] = ['#fff', '#FFE55C']
 
 if (followingBtn) {
     followingBtn.addEventListener('mouseover', () => {
@@ -49,58 +42,6 @@ createPostWrapper.addEventListener('click', (e) => {
     }
 })
 
-let lastClicked = 'climb'
-
-climbPost.addEventListener('click', () => {
-    lastClicked = 'climb'
-    climbPost.style.background = purple
-    partnerPost.style.background = 'none'
-    climbForm.style.display = "flex"
-    connectForm.style.display = "none"
-    partnerTitle.style.color = lightGray
-})
-
-climbPost.addEventListener('mouseover', () => {
-    if (lastClicked === 'climb') {
-        climbTitle.style.color = darkPurple
-    } else {
-        climbTitle.style.color = white
-    }
-})
-
-climbPost.addEventListener('mouseout', () => {
-    if (lastClicked === 'climb') {
-        climbTitle.style.color = white
-    } else {
-        climbTitle.style.color = lightGray
-    }
-})
-
-partnerPost.addEventListener('click', () => {
-    lastClicked = 'partner'
-    climbPost.style.background = 'none'
-    partnerPost.style.background = purple
-    connectForm.style.display = "flex"
-    climbForm.style.display = "none"
-    climbTitle.style.color = lightGray
-})
-
-partnerPost.addEventListener('mouseover', () => {
-    if (lastClicked === 'partner') {
-        partnerTitle.style.color = darkPurple
-    } else {
-        partnerTitle.style.color = white
-    }
-})
-
-partnerPost.addEventListener('mouseout', () => {
-    if (lastClicked === 'partner') {
-        partnerTitle.style.color = white
-    } else {
-        partnerTitle.style.color = lightGray
-    }
-})
-
 stars.forEach((star, i) => {
     star.addEventListener('click', () => {
         stars.slice(1).forEach(star => star.style.color = white)
@@ -108,6 +49,7 @@ stars.forEach((star, i) => {
         // color stars before
         for (let j = 0; j <= i; j++) {
             stars[j].style.color = gold
+            console.log(stars[j])
         }
     })
 })
