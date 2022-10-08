b6b5969574b2ad77d287b78b6fc9487c86b68dca
brianschnee <brian.a.schnee@gmail.com>
2022-10-08 16:51:47 -0400

    Fix: removed connect schema and made climbschema the default post

--- models/Post.js
+++ models/Post.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const ClimbSchema = new mongoose.Schema({
+const PostSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
@@ -40,35 +40,4 @@ const ClimbSchema = new mongoose.Schema({
     },
 })
 
-const ConnectSchema = new mongoose.Schema({
-    area: {
-        type: String,
-        required: true,
-        trim: true,
-    },
-    message: {
-        type: String,
-        required: true,
-        trim: true,
-        maxLength: 1000,
-    },
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-    },
-    comments: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Comment'
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now,
-    },
-})
-
-const PostSchema = {
-    Climb: mongoose.model('Climb', ClimbSchema),
-    Connect: mongoose.model('Connect', ConnectSchema)
-}
-
-module.exports = PostSchema
+module.exports = mongoose.model('Post', PostSchema)
