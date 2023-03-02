f9bf8722cc5d8560e0eb7be17ca22217baef9f64
brianschnee <brian.a.schnee@gmail.com>
2023-03-01 20:54:08 -0500

    route/handler for demo user

--- src/routes/main.ts
+++ src/routes/main.ts
@@ -1,6 +1,6 @@
 import express from 'express'
 import authControllers from '../controllers/auth'
-const { getLogin, postLogin, logout, getSignup, postSignup } = authControllers
+const { getLogin, postLogin, postDemo, logout, getSignup, postSignup } = authControllers
 import homeControllers from '../controllers/home'
 const { getIndex, getFAQ } = homeControllers
 
@@ -10,6 +10,7 @@ router.get('/', getIndex)
 router.get('/FAQ', getFAQ)
 router.get('/login', getLogin)
 router.post('/login', postLogin)
+router.post('/demo', postDemo)
 router.get('/logout', logout)
 router.get('/signup', getSignup)
 router.post('/signup', postSignup)
